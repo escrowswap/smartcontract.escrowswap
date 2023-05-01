dfb537c12087f84b30f443d1ed506afb059b6ff8
escrowswap <escrowswap@proton.me>
2023-05-01 07:40:34 +0200

    changed solidity compiler version

--- src/escrowswap_v1.0.sol
+++ src/escrowswap_v1.0.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 import "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
